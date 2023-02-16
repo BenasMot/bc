a8a7635f2d04655be55f6fd29dbfbcbf1adaa7f5
BenasMot <benasmotiejunas.bm@gmail.com>
2023-02-16 23:04:26 +0200

    add missing sendMesage call

--- src/client/senders/sendBlockBroadcastResponse.ts
+++ src/client/senders/sendBlockBroadcastResponse.ts
@@ -1,16 +1,16 @@
 import { StandardWebSocketClient } from 'https://deno.land/x/websocket@v0.1.4/mod.ts';
 import { BlockBroadcastResponseMessage } from '../message.ts';
+import { sendMessage } from './sendMessage.ts';
 
 export const sendBlockBroadcastResponse = async (
   socket: StandardWebSocketClient,
   verified: boolean,
   checkedBlockHash: string,
 ) => {
-  console.log('### Sending Block Broadcast Response', { verified });
   const message: BlockBroadcastResponseMessage = {
     type: 'BLOCK_BROADCAST_RESPONSE',
     verified,
     checkedBlockHash,
   };
-  await socket.send(JSON.stringify(message));
+  await sendMessage(socket, message);
 };
